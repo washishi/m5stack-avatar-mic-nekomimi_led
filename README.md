b3129e660a1a6057b0c6252cc2ed4571fb9822b6
washishi <washishi@hotmail.co.jp>
2024-04-29 09:43:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+
+# m5stack-avatar-mic-nekomimi
+[Nekomimi LED](https://github.com/washishi/nekomimi_led) のサンプルプログラムです  
+音に合わせてAvatarが口パクしたり、傾いたりすると共にNekomimi LEDが音量に応じて光ります  
+またボタンAを押すと画面の色が変わります、ダブルクリックすると上下が反転します  
+なおｽﾀｯｸﾁｬﾝに入れてもサーボの制御はしていないためサーボは動きません  
+
+※本プログラムは Takao Akaki (mongonta0716) さんの [m5stack-avatar-mic](https://github.com/mongonta0716/m5stack-avatar-mic) を改変したものです  
+　またそれを改変した 
+robo8080さんの [m5stack-avatar-mic-led](https://github.com/robo8080/m5stack-avatar-mic-led) も参考にさせてもらっています
+
+# 環境
+VSCode + PlatformIO  
+
+# 使用ライブラリ等 
+詳細はplatformio.iniを見てください  
+・espressif32@6.5.0  
+・M5Stack-Avatar@0.9.2  
+・M5Unified@0.1.13  
+・FastLED@3.6.0  
+・gob_unifiedButton (CoreS3のみ)
+
+# 対応デバイス
+
+- M5S5tack  BASIC + M5GO BOTTOM または M5GO か FIRE
+  - env:M5Stack_BASIC-M5GO_BOTTOM を選択
+  - マイクはM5GO BOTTOM のアナログマイクを利用します(GPIO34をAD変換)
+  - Nekomimi LEDはPortBに接続(O端子を利用、G26)
+
+- M5S5tack BASIC + PDMマイクユニット
+  - env:M5Stack_BASIC-PDM_PortA を選択
+  - PDMマイクユニット を PortAに接続
+  - Nekomimi LED は GND,+5V,G26 に何とかして接続してください
+
+- M5S5tack BASIC + CORE2_Ext._board等
+  - env:M5Stack_BASIC-PDM_I2S を選択
+  - CORE2_Expansion_boardをCORE2より借用して接続しそのPDMマイクを利用  
+    またはそのマイクの互換品をなんとかして接続(GND,+3.3v,G0:CLK,G34:DAT)
+  - Nekomimi LED は GND,+5V,G26 に何とかして接続してください
+    
+- CORE2 + M5GO BOTTOM2 または CORE2 FOR AWS
+  - env:M5Stack_CORE2-M5GO_BOTTOM2 を選択
+  - マイクはM5GO BOTTOM2 のPDMマイクを利用します(G0:CLK,G34:DAT)
+  - Nekomimi LEDはPortBに接続(O端子を利用、G26)
+
+- CORE2 (PortAにNekomimi LEDを取り付け)
+  - env:M5Stack_CORE2-LED_PortA を選択
+  - マイクはCORE2_Expansion_boardのPDMマイクを利用(GND,+3.3v,G0:CLK,G34:DAT)
+  - Nekomimi LEDはPortAに接続(DATA端子を利用、G32)
+
+- CORE2 + PDMマイクユニット(PortA)
+  - env:M5Stack_CORE2-PDM_PortA を選択
+  - PDMマイクユニット を PortAに接続
+  - Nekomimi LED は GND,+5V,G26 に何とかして接続してください
+
+- CoreS3 + DIN BASE もしくは M5GO3 Bottom
+  - env:M5Stack_CoreS3 を選択
+  - マイクはCoreS3内蔵のマイクを利用します
+  - Nekomimi LEDはPortBに接続(O端子を利用、G9)
+
+- M5StickC または M5StickC Plus
+  - env:M5StickC を選択
+  - Nekomimi LED は GROVE PORTに接続(G32)
+
+- M5StickC Plus2
+  - env:M5StickC_PLUS2 を選択
+  - Nekomimi LED は GROVE PORTに接続(G32)
+
+- AtomS3 + PDMマイクユニット
+  - env:M5AtomS3-PDM を選択
+  - PDMマイクユニット を GROVE PORTに接続
+  - Nekomimi LED は GND,+5V,G8 に何とかして接続してください
+
+- M5Dial + PDMマイクユニット(PortA)
+  - env:M5Dial-PDM_PortA を選択
+  - PDMマイクユニット を PortAに接続
+  - Nekomimi LED は PortBに接続(O端子を利用、G2)  
+
+# LICENSE
+MIT  
+  ・[m5stack-avatar-mic-nekomimi](https://github.com/washishi/m5stack-avatar-mic-nekomimi_led/blob/main/LICENSE)  
+  ・[m5stack-avatar-mic (original)](https://github.com/mongonta0716/m5stack-avatar-mic/blob/main/LICENSE)
+# Author
+[washishi](https://github.com/washishi)
---
+++ src/main.cpp
@@ -0,0 +1,364 @@
+// Copyright(c) 2023 Takao Akaki (Oreginal m5stack-avatar-mic)
+// Copyright(c) 2024 washishi
+
+#include <M5Unified.h>
+#include <Avatar.h>
+#include "fft.hpp"
+#include <cinttypes>
+#if defined(ARDUINO_M5STACK_CORES3)
+  #include <gob_unifiedButton.hpp>
+  goblib::UnifiedButton unifiedButton;
+#endif
+#define USE_MIC
+
+#define USE_FASTLED
+// LEDをレベルメータとして使用する;
+#ifdef USE_FASTLED
+#include "FastLED.h"
+
+#define USE_NEKOMIMI
+// Nekomimi LED (9 9 straight) を使用する;
+
+#ifdef USE_NEKOMIMI
+  #define NUM_LEDS 18
+  CRGB leds[NUM_LEDS];
+  CRGB led_table[NUM_LEDS/2] = {CRGB::Purple,CRGB::MediumPurple,CRGB::Blue,CRGB::Green,CRGB::LimeGreen,CRGB::Yellow,CRGB::DarkOrange,CRGB::OrangeRed,CRGB::Red};
+ #endif
+
+void turn_off_led() {
+  // Now turn the LED off, then pause
+  for(int i=0;i<NUM_LEDS;i++) leds[i] = CRGB::Black;
+  FastLED.show();
+}
+
+void fill_led_buff(CRGB color) {
+  // Now turn the LED off, then pause
+  for(int i=0;i<NUM_LEDS;i++) leds[i] =  color;
+}
+
+void clear_led_buff() {
+  // Now turn the LED off, then pause
+  for(int i=0;i<NUM_LEDS;i++) leds[i] =  CRGB::Black;
+}
+
+void level_led(int level1, int level2) {
+  if(level1>NUM_LEDS/2) level1 = NUM_LEDS/2;
+  if(level2>NUM_LEDS/2) level2 = NUM_LEDS/2;
+  clear_led_buff();
+  // LEDの順番
+  //
+  // M5GO Bottom2
+  //    下 ------------------ 上
+  // LED4 LED3 LED2 LED1 LED0
+  // LED5 LED6 LED7 LED8 LED9
+  //
+  // Nekomimi
+  // LED0 LED1  LED2  LED3  LED4  LED5  LED6  LED7　LED8
+  // LED9 LED10 LED11 LED12 LED13 LED14 LED15 LED16 LED17
+  
+  // 右LED 
+  for(int i=0;i<level1;i++){
+#ifndef USE_NEKOMIMI
+    leds[NUM_LEDS/2-1-i] = led_table[i];
+#else
+    leds[i] = led_table[i];
+    leds[8-i] = led_table[i];
+#endif
+  }
+  // 左LED
+  for(int i=0;i<level2;i++){
+    leds[i+NUM_LEDS/2] = led_table[i];
+    leds[8-i+NUM_LEDS/2] = led_table[i];
+  }
+  FastLED.show();
+}
+#endif
+
+#ifdef USE_MIC
+  // ---------- Mic sampling ----------
+
+  #define READ_LEN    (2 * 256)
+  #define LIPSYNC_LEVEL_MAX 10.0f
+
+  int16_t *adcBuffer = NULL;
+  static fft_t fft;
+  static constexpr size_t WAVE_SIZE = 256 * 2;
+
+  static constexpr const size_t record_samplerate = 16000; // M5StickCPlus2だと48KHzじゃないと動かなかったが、M5Unified0.1.12で修正されたのとM5AtomS2+PDMUnitで不具合が出たので戻した。。
+  static int16_t *rec_data;
+  
+  // setupの最初の方の機種判別で書き換えている場合があります。そちらもチェックしてください。（マイクの性能が異なるため）
+  uint8_t lipsync_shift_level = 11; // リップシンクのデータをどのくらい小さくするか設定。口の開き方が変わります。
+  float lipsync_max =LIPSYNC_LEVEL_MAX;  // リップシンクの単位ここを増減すると口の開き方が変わります。
+
+#endif
+
+
+
+
+using namespace m5avatar;
+
+Avatar avatar;
+ColorPalette *cps[6];
+uint8_t palette_index = 0;
+
+uint32_t last_rotation_msec = 0;
+uint32_t last_lipsync_max_msec = 0;
+uint8_t display_rotation = 1; // ディスプレイの向き(0〜3)
+
+void lipsync() {
+  
+  size_t bytesread;
+  uint64_t level = 0;
+#ifndef SDL_h_
+  if ( M5.Mic.record(rec_data, WAVE_SIZE, record_samplerate)) {
+    fft.exec(rec_data);
+    for (size_t bx=5;bx<=60;++bx) {
+      int32_t f = fft.get(bx);
+      level += abs(f);
+    }
+  }
+  uint32_t temp_level = level >> lipsync_shift_level;
+  //M5_LOGI("level:%" PRId64 "\n", level) ;         // lipsync_maxを調整するときはこの行をコメントアウトしてください。
+  //M5_LOGI("temp_level:%d\n", temp_level) ;         // lipsync_maxを調整するときはこの行をコメントアウトしてください。
+  float ratio = (float)(temp_level / lipsync_max);
+  //M5_LOGI("ratio:%f\n", ratio);
+  if (ratio <= 0.01f) {
+    ratio = 0.0f;
+    if ((lgfx::v1::millis() - last_lipsync_max_msec) > 500) {
+      // 0.5秒以上無音の場合リップシンク上限をリセット
+      last_lipsync_max_msec = lgfx::v1::millis();
+      lipsync_max = LIPSYNC_LEVEL_MAX;
+    }
+  } else {
+    if (ratio > 1.3f) {
+      if (ratio > 1.5f) {
+        // リップシンク上限を大幅に超えた場合、上限を上げていく。
+        lipsync_max += 10.0f;
+      }
+      ratio = 1.3f;
+    }
+    last_lipsync_max_msec = lgfx::v1::millis(); // 無音でない場合は更新
+  }
+
+  if ((lgfx::v1::millis() - last_rotation_msec) > 350) {
+    int direction = random(-2, 2);
+    avatar.setRotation(direction * 10 * ratio);
+    last_rotation_msec = lgfx::v1::millis();
+  }
+#else
+  float ratio = 0.0f;
+#endif
+  avatar.setMouthOpenRatio(ratio);
+
+#ifdef USE_FASTLED
+   int led_level = (int)(ratio*(NUM_LEDS/2));
+   level_led(led_level, led_level);
+#endif 
+}
+
+
+void setup()
+{
+  auto cfg = M5.config();
+#if defined( PDM_PORTA )||( PDM_I2S )
+  cfg.internal_mic = false;
+#else
+  cfg.internal_mic = true;
+#endif  
+  M5.begin(cfg);
+#if defined( ARDUINO_M5STACK_CORES3 )
+  unifiedButton.begin(&M5.Display, goblib::UnifiedButton::appearance_t::transparent_all);
+#endif
+  M5.Log.setLogLevel(m5::log_target_display, ESP_LOG_NONE);
+  M5.Log.setLogLevel(m5::log_target_serial, ESP_LOG_INFO);
+  M5.Log.setEnableColor(m5::log_target_serial, false);
+  M5_LOGI("Avatar Start");
+  M5.Log.printf("M5.Log avatar Start\n");
+  float scale = 0.0f;
+  int8_t position_top = 0;
+  int8_t position_left = 0;
+//uint8_t display_rotation = 1; // ディスプレイの向き(0〜3)
+  uint8_t first_cps = 0;
+  auto mic_cfg = M5.Mic.config();
+    switch (M5.getBoard()) {
+    case m5::board_t::board_M5AtomS3:
+      first_cps = 4;
+      scale = 0.55f;
+      position_top =  -60;
+      position_left = -95;
+      display_rotation = 2;
+      // M5AtomS3は外部マイク(PDMUnit)なので設定を行う。
+      mic_cfg.sample_rate = 16000;
+      //mic_cfg.dma_buf_len = 256;
+      //mic_cfg.dma_buf_count = 3;
+      mic_cfg.pin_ws = 1;
+      mic_cfg.pin_data_in = 2;
+      M5.Mic.config(mic_cfg);
+      break;
+
+    case m5::board_t::board_M5StickC:
+      first_cps = 1;
+      scale = 0.6f;
+      position_top = -80;
+      position_left = -80;
+      display_rotation = 3;
+      break;
+
+    case m5::board_t::board_M5StickCPlus:
+      first_cps = 1;
+      scale = 0.85f;
+      position_top = -55;
+      position_left = -35;
+      display_rotation = 3;
+      break;
+
+    case m5::board_t::board_M5StickCPlus2:
+      first_cps = 2;
+      scale = 0.85f;
+      position_top = -55;
+      position_left = -35;
+      display_rotation = 3;
+      break;
+   
+     case m5::board_t::board_M5StackCore2:
+      scale = 1.0f;
+      position_top = 0;
+      position_left = 0;
+      display_rotation = 1;
+      // 外部マイク設定
+#if defined( PDM_PORTA )
+      mic_cfg.pin_ws = 33;
+      mic_cfg.pin_data_in = 32;
+      M5.Mic.config(mic_cfg);
+#endif
+      break;
+
+    case m5::board_t::board_M5StackCoreS3:
+      scale = 1.0f;
+      position_top = 0;
+      position_left = 0;
+      display_rotation = 1;
+      break;
+
+    case m5::board_t::board_M5Stack:
+      scale = 1.0f;
+      position_top = 0;
+      position_left = 0;
+      display_rotation = 1;
+      // 外部マイク設定
+#if defined( PDM_PORTA )
+      M5.In_I2C.release();
+      mic_cfg.pin_ws = 22;
+      mic_cfg.pin_data_in = 21;
+      M5.Mic.config(mic_cfg);
+#endif
+#if defined( PDM_I2S )
+      mic_cfg.pin_ws = 0;
+      mic_cfg.pin_data_in = 34;
+      M5.Mic.config(mic_cfg);
+#endif
+      break;
+
+    case m5::board_t::board_M5Dial:
+      first_cps = 1;
+      scale = 0.8f;
+      position_top =  0;
+      position_left = -40;
+      display_rotation = 2;
+      // M5ADial(StampS3)は外部マイク(PDMUnit)なので設定を行う。(Port.A)
+      mic_cfg.pin_ws = 15;
+      mic_cfg.pin_data_in = 13;
+      M5.Mic.config(mic_cfg);
+      break;
+
+      
+    defalut:
+      M5.Log.println("Invalid board.");
+      break;
+  }
+#ifndef SDL_h_
+  rec_data = (typeof(rec_data))heap_caps_malloc(WAVE_SIZE * sizeof(int16_t), MALLOC_CAP_8BIT);
+  memset(rec_data, 0 , WAVE_SIZE * sizeof(int16_t));
+  M5.Mic.begin();
+#endif
+  M5.Speaker.end();
+
+  M5.Display.setRotation(display_rotation);
+  avatar.setScale(scale);
+  avatar.setPosition(position_top, position_left);
+  avatar.init(1); // start drawing
+  cps[0] = new ColorPalette();
+  cps[0]->set(COLOR_PRIMARY, TFT_BLACK);
+  cps[0]->set(COLOR_BACKGROUND, TFT_YELLOW);
+  cps[1] = new ColorPalette();
+  cps[1]->set(COLOR_PRIMARY, TFT_BLACK);
+  cps[1]->set(COLOR_BACKGROUND, TFT_ORANGE);
+  cps[2] = new ColorPalette();
+  cps[2]->set(COLOR_PRIMARY, (uint16_t)0x00ff00);
+  cps[2]->set(COLOR_BACKGROUND, (uint16_t)0x303303);
+  cps[3] = new ColorPalette();
+  cps[3]->set(COLOR_PRIMARY, TFT_WHITE);
+  cps[3]->set(COLOR_BACKGROUND, TFT_BLACK);
+  cps[4] = new ColorPalette();
+  cps[4]->set(COLOR_PRIMARY, TFT_BLACK);
+  cps[4]->set(COLOR_BACKGROUND, TFT_WHITE);
+  cps[5] = new ColorPalette();
+  cps[5]->set(COLOR_PRIMARY, (uint16_t)0x303303);
+  cps[5]->set(COLOR_BACKGROUND, (uint16_t)0x00ff00);
+  avatar.setColorPalette(*cps[first_cps]);
+  //avatar.addTask(lipsync, "lipsync");
+  last_rotation_msec = lgfx::v1::millis();
+
+#ifdef USE_FASTLED
+  FastLED.addLeds<SK6812, LED_GPIO, GRB>(leds, NUM_LEDS);  // GRB ordering is typical
+  FastLED.setBrightness(10);
+  level_led(NUM_LEDS/2, NUM_LEDS/2);
+  FastLED.show();
+  delay(500);
+#endif
+  M5_LOGI("setup end");
+}
+
+uint32_t count = 0;
+
+void loop()
+{
+  M5.update();
+
+#if defined( ARDUINO_M5STACK_CORES3 )
+  unifiedButton.update();
+#endif
+  if (M5.BtnA.wasPressed()) {
+    M5_LOGI("Push BtnA");
+    palette_index++;
+    if (palette_index > 5) {
+      palette_index = 0;
+    }
+    avatar.setColorPalette(*cps[palette_index]);
+  }
+  if (M5.BtnA.wasDoubleClicked()) {
+//  M5.Display.setRotation(3);
+    display_rotation+=2;
+    if (display_rotation > 3){
+      display_rotation %= 4;
+    }
+    M5.Display.setRotation(display_rotation);
+  }
+  if (M5.BtnPWR.wasClicked()) {
+#ifdef ARDUINO
+    esp_restart();
+#endif
+  } 
+//  if ((millis() - last_rotation_msec) > 100) {
+    //float angle = 10 * sin(count);
+    //avatar.setRotation(angle);
+    //last_rotation_msec = millis();
+    //count++;
+  //}
+
+  // avatar's face updates in another thread
+  // so no need to loop-by-loop rendering
+  lipsync();
+  lgfx::v1::delay(1);
+}
